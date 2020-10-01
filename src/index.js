df66e8ac7abb8c8443fd8ba7a50f570435e3dfa1
KarinaOknup <punko.karina@yandex.by>
2020-10-02 01:08:39 +0300

    add: first try with code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(Array.isArray(array)){ //проверка на сущ-ние массива
+    let l = array.length ;//кол-во элементов
+    let min = array[0];//начальный минимальный
+    if(array.length===0){
+      return 0;
+    }
+    else{
+      for  (let i = 0; i <= l; i++ ) {
+        if (min > array[i]) min=array[i];
+      }
+      return min;
+  }
+  }
+  else  return 0 ;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(Array.isArray(array)){ //проверка на сущ-ние массива
+    let l = array.length ;//кол-во элементов
+    let max = array[0];//начальный максимальный
+        if(array.length === 0){
+          return 0;
+        }
+        else{
+          for  (let i = 0; i <= l; i++ ) {
+            if (max > array[i]) max = array[i];
+          }
+          return max ;
+      }
+  }
+  else  return 0 ;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(Array.isArray(array)){//проверка на сущ-ние массива
+    let sum=0
+    for  (let i = 0; i <= array.length; i++ ) { 
+      sum+=array[i];
+    }
+    return sum/array.length;
+}else return 0;
 }
